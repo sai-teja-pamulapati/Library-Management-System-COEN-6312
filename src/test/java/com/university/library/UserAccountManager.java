37f4b87d181a8e809b66f2a5f4ed20ab9b352f5f
Abhinaw Siddharth Bogadhi <65751192+abhinawsiddharth@users.noreply.github.com>
2024-02-18 16:42:46 -0500

    Update UserAccountManager.java

--- src/test/java/com/university/library/UserAccountManager.java
+++ src/test/java/com/university/library/UserAccountManager.java
@@ -4,13 +4,18 @@ import java.util.Map;
 public class UserAccountManager {
     private Map<String, User> userAccounts = new HashMap<>();
 
-    public boolean createUserAccount(String username, String password) {
+    public UserAccountManager() {
+        userAccounts.put("admin", new User("admin", "admin123", User.UserRole.ADMIN));
+    }
+
+    public boolean createUserAccount(String username, String password, User.UserRole role) {
         if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
             System.out.println("Username and password cannot be empty");
             return false;
         }
         if (userAccounts.containsKey(username)) {
             System.out.println("User already exists");
+            
             return false;
         }
 
@@ -19,16 +24,15 @@ public class UserAccountManager {
             return false;
         }
 
-        User newUser = new User(username, password);
+        User newUser = new User(username, password, role);
         userAccounts.put(username, newUser);
-        System.out.println("User account created successfully for username: " + username);
+        System.out.println("User account created successfully for username: " + username + " Role: " + role);
         return true;
-
     }
 
     public boolean doesUsernameExist(String username) {
         return userAccounts.containsKey(username);
-    }    
+    }
 
     public boolean verifyLogin(String username, String password) {
         if (username == null || password == null) {
