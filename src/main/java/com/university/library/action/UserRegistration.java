93351de0852989d566ba878cbadbb1fb98ace0ab
Abhinaw Siddharth Bogadhi <65751192+abhinawsiddharth@users.noreply.github.com>
2024-02-20 18:54:27 -0500

    Update UserRegistration.java

--- src/main/java/com/university/library/action/UserRegistration.java
+++ src/main/java/com/university/library/action/UserRegistration.java
@@ -1,5 +1,6 @@
 package com.university.library.action;
 
+import java.io.Console;
 import java.util.Scanner;
 
 import com.university.library.model.User;
@@ -12,6 +13,9 @@ public class UserRegistration {
     private static UserRepository userRepository = UserRepository.getInstance();
 
     public static void register() {
+
+        Console console = System.console();
+
         System.out.println("Please enter your name:");
         String name = scanner.nextLine();
         
@@ -19,7 +23,7 @@ public class UserRegistration {
         String emailId = scanner.nextLine();
         
         System.out.println("Please enter your password:");
-        String password = scanner.nextLine();
+        String password = new String(console.readPassword());
         
         System.out.println("Please enter your mobile number:");
         String mobileNumber = scanner.nextLine();
@@ -30,11 +34,56 @@ public class UserRegistration {
         System.out.println("Please enter your date of birth (YYYY-MM-DD):");
         String dateOfBirth = scanner.nextLine();
         
-        System.out.println("Please enter your gender (Male/Female/Other):");
-        String gender = scanner.nextLine();
-        
-        System.out.println("Please select your role (ADMIN, STUDENT, STAFF, LIBRARIAN, FREE_USER, PAID_USER):");
-        UserRole userRole = UserRole.valueOf(scanner.nextLine().toUpperCase());
+        String gender = "";
+        while(true) {
+            try {
+                System.out.print("Please select your gender:\n" +
+                    "1. Male\n" +
+                    "2. Female\n" +
+                    "3. Other\n");
+                String genderOption = scanner.nextLine();
+                switch (genderOption) {
+                    case "1":
+                        gender = "Male";
+                        break;
+                    case "2":
+                        gender = "Female";
+                        break;
+                    case "3":
+                        gender = "Other";
+                        break;
+                    default:
+                        System.out.println("Invalid option!");
+                        continue;
+                    }
+                } catch (Exception e) {
+                System.out.println(e.getLocalizedMessage());
+                return;
+            }
+            break;
+        }
+
+        UserRole userRole = null;
+        while (true) {
+            System.out.println("Please select your role:");
+            UserRole[] roles = UserRole.values();
+            for (int i = 0; i < roles.length; i++) {
+                System.out.println((i + 1) + ". " + roles[i].name());
+            }
+            try {
+                int roleOption = Integer.parseInt(scanner.nextLine()) - 1;
+                if (roleOption >= 0 && roleOption < roles.length) {
+                    userRole = roles[roleOption];
+                } else {
+                    System.out.println("Invalid selection. Please try again.");
+                    continue;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                continue;
+            }
+            break;
+        }
 
         User user = new User();
         user.setName(name);
