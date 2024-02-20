069989b3c79b2977b8c8945e3c26c9ef66d7b641
Abhinaw Siddharth Bogadhi <65751192+abhinawsiddharth@users.noreply.github.com>
2024-02-20 13:10:04 -0500

    Updated App.java

--- src/main/java/com/university/library/App.java
+++ src/main/java/com/university/library/App.java
@@ -1,45 +1,41 @@
 package com.university.library;
-import com.university.library.action.UserLogin;
-
 import java.util.Scanner;
-
+import com.university.library.action.UserLogin;
+import com.university.library.action.UserRegistration;
 
 public class App {
 
-	private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		while (true) {
-			try {
-				initializeSystem();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-		}
-	}
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                initializeSystem();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 
-	/**
-	 * 
-	 */
-	private static void initializeSystem() {
-		System.out.print("Welcome to Library Management System \n" +
-				"Choose from the following options. \n" +
-				"1. User Login \n" +
-				"2. User Registration \n" +
-				"3. Exit \n");
-		String firstCommand = scanner.nextLine();
-		switch (firstCommand) {
-			case "1":
-				UserLogin.login();
-				break;
-			case "2":
-				UserLogin.register();
-				break;
-			case "3":
-				System.out.println("Bye");
-				System.exit(0);
-			default:
-				throw new IllegalArgumentException("Invalid option!");
-		}
-	}
+    private static void initializeSystem() {
+        System.out.print("Welcome to Library Management System \n" +
+                "Choose from the following options. \n" +
+                "1. User Login \n" +
+                "2. User Registration \n" +
+                "3. Exit \n");
+        String firstCommand = scanner.nextLine();
+        switch (firstCommand) {
+            case "1":
+                UserLogin.login();
+                break;
+            case "2":
+                UserRegistration.register();
+                break;
+            case "3":
+                System.out.println("Bye");
+                System.exit(0);
+            default:
+                throw new IllegalArgumentException("Invalid option!");
+        }
+    }
 }
