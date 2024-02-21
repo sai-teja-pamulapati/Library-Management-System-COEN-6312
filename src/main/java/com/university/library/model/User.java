22a66a7986515f0811cf59624185cb950cf0ee6e
Abhinaw Siddharth Bogadhi <65751192+abhinawsiddharth@users.noreply.github.com>
2024-02-21 11:21:07 -0500

    Updated model to add sample users.

--- src/main/java/com/university/library/model/User.java
+++ src/main/java/com/university/library/model/User.java
@@ -8,10 +8,23 @@ public class User {
     private String address;
     private UserRole userRole;
     private String dateOfBirth;
-    private String Gender;
+    private String gender;
+
+    public User() {}
+
+    public User(String name, String emailId, String password, String mobileNumber, 
+                String address, String dateOfBirth, String gender, UserRole userRole) {
+        this.name = name;
+        this.emailId = emailId;
+        this.password = password;
+        this.mobileNumber = mobileNumber;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.userRole = userRole;
+    }
 
     public static User login(String emailId , String password) {
-        // TODO code
         User user = new User();
         user.setUserRole(UserRole.STUDENT);
         return user;
@@ -74,10 +87,10 @@ public class User {
     }
 
     public String getGender() {
-        return Gender;
+        return gender;
     }
 
     public void setGender(String gender) {
-        Gender = gender;
+        this.gender = gender;
     }
 }
