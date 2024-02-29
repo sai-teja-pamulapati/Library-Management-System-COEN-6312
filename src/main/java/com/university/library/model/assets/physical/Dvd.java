000be559410b5871f53a53351d681ad33a3f64e9
kavy5994 <116515170+kavy5994@users.noreply.github.com>
2024-02-28 19:21:40 -0500

    new classes added

---
+++ src/main/java/com/university/library/model/assets/physical/Dvd.java
@@ -0,0 +1,34 @@
+package com.university.library.model.assets.physical;
+
+public class Dvd extends PhysicalAsset {
+    private String storage;
+    private String condition;
+    private String manufacturer;
+
+    public String getstorage() {
+        return storage;
+    }
+
+    public void setstorage(String storage) {
+        this.storage = storage;
+    }
+
+    public String getcondition() {
+        return condition;
+    }
+
+    public void setcondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getmanufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return "DVD [storage " + storage +
+                ", condition= " + condition +
+                ", manufacturer= " + manufacturer + "]";
+    }
+}
