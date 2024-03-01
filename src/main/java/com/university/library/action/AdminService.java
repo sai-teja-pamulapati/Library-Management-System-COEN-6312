12e47ab5410ddee18c4c3b43059c8128f1488243
Pradheep Shankar <pradheep.shankar@mail.concordia.ca>
2024-03-01 16:26:35 -0500

    User as toString() pulled into adminService

--- src/main/java/com/university/library/action/AdminService.java
+++ src/main/java/com/university/library/action/AdminService.java
@@ -16,13 +16,7 @@ public class AdminService {
             System.out.println("No users found.");
         } else {
             for (User user : users) {
-                System.out.println("\nUserID: " + user.getUserId() +
-                                    ", Name: " + user.getName() +
-                                    ", Email: " + user.getEmailId() +
-                                    ", Mobile: " + user.getMobileNumber() +
-                                    ", Role: " + user.getUserRole() +
-                                    ", Blocked: " + (user.isBlocked() ? "Yes" : "No") +
-                                    "\n");
+                System.out.println(user);
 
             }
         }
