e908da42c9812e07726bdc535874adc5453d8e44
AS <abhinawsiddharth2000@gmail.com>
2024-03-03 14:23:10 -0500

    Updated user registration test cases.

--- src/test/java/com/university/library/action/UserRegistrationTest.java
+++ src/test/java/com/university/library/action/UserRegistrationTest.java
@@ -3,7 +3,6 @@ package com.university.library.action;
 import com.university.library.model.users.User;
 import com.university.library.model.users.UserRole;
 import com.university.library.repository.UserRepository;
-
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -14,22 +13,22 @@ public class UserRegistrationTest {
 
     @Before
     public void setUp() {
-        userRepository = UserRepository.getInstance(); 
+        userRepository = UserRepository.getInstance();
+        userRepository.clearUsers();
     }
 
     @Test
-    public void testAddUser1() {
-        // Addin new user.
+    public void testAddingUsers() {
+        // Adding a new user
         User user1 = new User("Mike Smith", "mike.smith@example.com", "password123", "1122334455", "789 Pine St", "03-03-1990", "Male", UserRole.ADMIN);
-        assertTrue(userRepository.addUser(user1));
+        assertTrue("User1 should be added successfully", userRepository.addUser(user1));
 
-        // User with same email ID.
+        // Attempting to add a user with the same email ID should fail
         User user2 = new User("Smith Mike", "mike.smith@example.com", "PPSSPP123123", "1234567890", "123 Yes St", "07-09-2000", "Male", UserRole.STUDENT);
-        assertFalse(userRepository.addUser(user2));
+        assertFalse("User2 with an existing email should not be added", userRepository.addUser(user2));
 
-        // Adding new user.
+        // Adding another new user with a unique email
         User user3 = new User("Sam Wilson", "sam.wilson@gmail.com", "sam123", "1122334455", "789 Pine St", "1993-03-03", "Other", UserRole.LIBRARIAN);
-        assertTrue(userRepository.addUser(user3));
+        assertTrue("User3 should be added successfully", userRepository.addUser(user3));
     }
-
 }
