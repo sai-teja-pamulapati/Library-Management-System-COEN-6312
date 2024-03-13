cab749a419fc16a61d7f920c5101d6b169cdb236
sai-teja-concordia <saiteja.pamulapati@mail.concordia.ca>
2024-03-13 13:20:28 -0400

    email id regex change

--- src/main/java/com/university/library/action/UserRegistration.java
+++ src/main/java/com/university/library/action/UserRegistration.java
@@ -21,7 +21,7 @@ public class UserRegistration {
     private static UserRepository userRepository = UserRepository.getInstance();
 
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
-            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("^[A-Z0-9]+@[A-Z0-9]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     public static void register(boolean selfRegistration) {
 
