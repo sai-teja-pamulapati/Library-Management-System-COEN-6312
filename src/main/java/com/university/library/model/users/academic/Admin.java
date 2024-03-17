db1f3d24874095ac9644af5fe23f5dd62a690e5c
kavy5994 <116515170+kavy5994@users.noreply.github.com>
2024-03-17 14:49:06 -0400

    including officehours ocl in admin

--- src/main/java/com/university/library/model/users/academic/Admin.java
+++ src/main/java/com/university/library/model/users/academic/Admin.java
@@ -8,8 +8,10 @@ public class Admin extends AcademicUser {
     private String officeHours;
     private String contractType;
 
-    public Admin(String userId, String name , String emailId , String password , String mobileNumber , String address , String dateOfBirth , String gender , String universityId , String issueDate , String officeLocation , String officeHours , String contractType) {
-        super(userId, name , emailId , password , mobileNumber , address , dateOfBirth , gender , universityId , issueDate);
+    public Admin(String userId, String name, String emailId, String password, String mobileNumber, String address,
+            String dateOfBirth, String gender, String universityId, String issueDate, String officeLocation,
+            String officeHours, String contractType) {
+        super(userId, name, emailId, password, mobileNumber, address, dateOfBirth, gender, universityId, issueDate);
         this.officeLocation = officeLocation;
         this.officeHours = officeHours;
         this.contractType = contractType;
@@ -41,12 +43,17 @@ public class Admin extends AcademicUser {
 
     public boolean isIssueDateValid() {
         LocalDate today = LocalDate.now();
-        LocalDate issueDate = LocalDate.parse(this.getIssueDate()); // Assuming getIssueDate returns a String in the format "yyyy-MM-dd"
+        LocalDate issueDate = LocalDate.parse(this.getIssueDate()); // Assuming getIssueDate returns a String in the
+                                                                    // format "yyyy-MM-dd"
         return !issueDate.isAfter(today);
+    }
+
+    public void updateOfficehours(String officeHours) {
+        this.officeHours = officeHours;
     }
 
     @Override
     public String toString() {
-        return getClass().getName() +"\n"+ super.toString();
+        return getClass().getName() + "\n" + super.toString();
     }
 }
